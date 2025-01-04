3a2e37a91e29dc772581f90c381b0a59c1c3c434
Pepega Vsevolodovich <pepega@sevap.ru>
2025-01-05 00:17:57 +0100

    Refactor main function to process chats and save embeddings, updating cluster size and output formats

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-import os
+import pickle
+import polars as pl
 from TelegramPreprocessor import TelegramPreprocessor
 
 def main():
-            
-
+    # file_path = '/home/seva/data/all_chats.parquet'
+                
+    # df = pl.read_parquet(file_path)
+    # df.shape
     file_path = '/home/seva/data/messages_json/result.json'
     time_window = '30m'
-    cluster_size = 3
+    cluster_size = 1
     big_cluster_size = 10
 
     preprocessor = TelegramPreprocessor(time_window=time_window, 
-                                        cluster_size=cluster_size, 
-                                        big_cluster_size=big_cluster_size)
+                                         cluster_size=cluster_size, 
+                                         big_cluster_size=big_cluster_size)
 
-    data = preprocessor.prepare_data(file_path)
+    data = preprocessor.process_chats(file_path, time_window=time_window)
     print(data)
+
+    #pickle.dump(data, open('/home/seva/data/all_chats.pkl', 'wb'))
+    combined_clusters = []
+    embeddings_dict = {}
+    for chat_id, chat_data in data.items():
+        combined_clusters.append(chat_data['clusters'])
+        embeddings_dict[chat_id] = chat_data['embeddings']
+        
+    big_dataframe = pl.concat(combined_clusters, how="vertical")
+    big_dataframe.write_parquet('/home/seva/data/all_chats.parquet')
+    pickle.dump(embeddings_dict, open('/home/seva/data/all_embeddings.pkl', 'wb'))
+    
+    
 
 main()
